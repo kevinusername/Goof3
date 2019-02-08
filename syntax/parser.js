142083485aff472e3ca0cfb04eb249f72c2cf383
Kevin Peters <you@example.com>
2019-02-07 17:46:52 -0800

    re added parser errors

--- syntax/parser.js
+++ syntax/parser.js
@@ -1,8 +1,11 @@
-// module "parser.js"
 var ohm = require('ohm-js')
 var fs = require('fs')
 const grammar = ohm.grammar(fs.readFileSync('./syntax/goof3.ohm'))
 
 module.exports = (text) => {
-  return grammar.match(text).succeeded()
+  const match = grammar.match(text)
+  if (!match.succeeded()) {
+    throw new Error(`Syntax Error: ${match.message}`)
+  }
+  return match.succeeded()
 }
