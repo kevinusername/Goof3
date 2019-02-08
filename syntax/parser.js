bd17fda985d7b98559b82ffa1250737a9511bab9
Kevin Peters <you@example.com>
2019-02-07 17:04:18 -0800

    simplified parser

--- syntax/parser.js
+++ syntax/parser.js
@@ -4,9 +4,5 @@ var fs = require('fs')
 const grammar = ohm.grammar(fs.readFileSync('./syntax/goof3.ohm'))
 
 module.exports = (text) => {
-  const match = grammar.match(text)
-  //   if (!match.succeeded()) {
-  //     console.log(match.message)
-  //   }
-  return match.succeeded()
+  return grammar.match(text).succeeded()
 }
