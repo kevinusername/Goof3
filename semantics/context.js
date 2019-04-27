7506bee20a76a34fa0bf022677dcabeb14b5f5eb
Kevin Peters <35283725+kevinusername@users.noreply.github.com>
2019-04-26 19:30:27 -0700

    minor cleanups

--- semantics/context.js
+++ semantics/context.js
@@ -10,28 +10,6 @@ const { standardFunctions } = require('./builtins');
 
 require('./analyzer');
 
-// When doing semantic analysis we pass around context objects.
-//
-// A context object holds:
-//
-//   1. A reference to the parent context (or null if this is the root context).
-//      This allows to search for declarations from the current context outward.
-//
-//   2. A reference to the current function we are analyzing, if any. If we are
-//      inside a function, then return expressions are legal, and we will be
-//      able to type check them.
-//
-//   3. Whether we are in a loop (to know that a `break` is okay).
-//
-//   4. A map for looking up types declared in this context.
-//
-//   5. A map for looking up vars and functions declared in this context.
-//
-// The reason for the two maps is that in Tiger, types are kept in a separate
-// namespace from all of the variables and functions. So you could declare a
-// type called "list" and a variable called "list" in the same scope. But you
-// probably shouldn't.
-
 class Context {
     constructor({ parent = null, currentFunction = null, inLoop = false } = {}) {
         Object.assign(this, {
@@ -51,15 +29,6 @@ class Context {
         // When entering a loop body, just set the inLoop field, retain others
         return new Context({ parent: this, currentFunction: this.currentFunction, inLoop: true });
     }
-
-    // createChildContextForBlock() {
-    //     // For a block, we have to retain both the function and loop settings.
-    //     return new Context({
-    //         parent: this,
-    //         currentFunction: this.currentFunction,
-    //         inLoop: this.inLoop,
-    //     });
-    // }
 
     createChildContextForObject() {
         return new Context({ parent: this, currentFunction: this.currentFunction, inLoop: false });
